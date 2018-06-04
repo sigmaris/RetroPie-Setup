79823cd2f493b120d5c187aacfa0337e7661b35b
Hugh Cole-Baker <sigmaris@gmail.com>
2018-06-04 15:30:12 +0100

    Fix error from merge

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -169,7 +169,6 @@ function get_retropie_depends() {
     if ! getDepends "${depends[@]}"; then
         fatalError "Unable to install packages required by $0 - ${md_ret_errors[@]}"
     fi
-}
 
     # make sure we don't have xserver-xorg-legacy installed as it breaks launching x11 apps from ES
     if ! isPlatform "x11" && hasPackage "xserver-xorg-legacy"; then
